502dace17b1fecaa5652484587b9efb0828055e6
Lines <youxi_lin@126.com>
2023-06-22 16:34:11 +0800

    1、添加支持vercel

--- app.ts
+++ app.ts
@@ -1,13 +1,7 @@
 #!/usr/bin/env node
-// import { startService } from './server';
-// async function  start () {
-//   await startService();
-// }
-//
-// start().then(() => {}).catch(e => console.log(e));
-
-async function start() {
-  require('./server').startService();
+import { startService } from './server';
+async function  start () {
+  await startService();
 }
 
-start();
+start().then(() => {}).catch(e => console.log(e));
