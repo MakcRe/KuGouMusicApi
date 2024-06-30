63b394fda3cdd123c591d98f1826c265597c2814
lines <youxi_lin@126.com>
2024-06-30 22:10:46 +0800

    1、获取歌手列表, issue:#26 2、更新文档

---
+++ module/artist_lists.js
@@ -0,0 +1,19 @@
+// 歌手列表
+module.exports = (params, useAxios) => {
+  const paramsMap = {
+    musician: Number(params.musician || 0),
+    sextype: params.sextypes || 0,
+    showtype: 2,
+    type: params.type || 0,
+    hotsize:  Number(params.hotsize || 30)
+  };
+
+  console.log(paramsMap);
+  return useAxios({
+    url: '/ocean/v6/singer/list',
+    method: 'GET',
+    params: paramsMap,
+    encryptType: 'android',
+    cookie: params?.cookie || {},
+  });
+};
