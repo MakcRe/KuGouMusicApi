2e0cd97037763000024aa757c07745a8db60721d
lines <youxi_lin@126.com>
2025-01-24 18:36:20 +0800

    更新 获取歌单所有歌曲 所有接口 #54

--- module/playlist_track_all.js
+++ module/playlist_track_all.js
@@ -1,20 +1,26 @@
 // 获取歌单所有歌曲
 module.exports = (params, useAxios) => {
+  const pagesize = params?.pagesize || 30;
   const paramsMap = {
+    area_code: 1,
+    begin_idx: (Number(params.page || 1) - 1) * pagesize,
     plat: 1,
     type: 1,
-    module: 'NONE',
-    page: params?.page || 1,
-    pagesize: params?.pagesize || 30,
+    // module: 'NONE',
+    mode: 1,
+    personal_switch: 1,
+    extend_fields: 'abtags,hot_cmt,popularization',
+    // page: params?.page || 1,
+    pagesize ,
     global_collection_id: params?.id,
   };
 
   return useAxios({
-    url: '/v4/get_other_list_file',
+    url: '/pubsongs/v2/get_other_list_file_nofilt',
     method: 'GET',
     encryptType: 'android',
     params: paramsMap,
     cookie: params?.cookie || {},
-    headers: { 'x-router': 'pubsongscdn.kugou.com' },
+    // headers: { 'x-router': 'pubsongscdn.kugou.com' },
   });
 };
