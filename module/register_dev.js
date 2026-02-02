20360486a3d13f262f8f81a26f83c06c723e506f
阿珏酱 <1638211921@qq.com>
2026-02-02 16:23:26 +0800

    优化Cookie处理逻辑，简化设置过程并通过dev返回设备信息

--- module/register_dev.js
+++ module/register_dev.js
@@ -1,10 +1,29 @@
 module.exports = (params, useAxios) => {
+  const cookie = params?.cookie || {};
+  const shouldRequest = !(params && Object.prototype.hasOwnProperty.call(params, 'register'));
   const dataMap = {
-    mid: params?.mid  || params?.cookie?.KUGOU_API_MID || '',
+    mid: params?.mid  || cookie?.KUGOU_API_MID || '',
     uuid: params?.uuid || '-',
     appid: '1014',
     userid: params?.userid || '0',
   };
+
+  if (!shouldRequest) {
+    return Promise.resolve({
+      status: 200,
+      body: {
+        status: 1,
+        data: {
+          mid: cookie?.KUGOU_API_MID,
+          guid: cookie?.KUGOU_API_GUID,
+          serverDev: cookie?.KUGOU_API_DEV,
+          mac: cookie?.KUGOU_API_MAC,
+        },
+      },
+      cookie: [],
+      headers: {},
+    });
+  }
 
   return new Promise((resolve, reject) => {
     useAxios({
@@ -14,14 +33,20 @@ module.exports = (params, useAxios) => {
       data: Buffer.from(JSON.stringify(dataMap)).toString('base64'),
       params: { ...dataMap, 'p.token': '', platid: 4 },
       encryptType: 'register',
-      cookie: params?.cookie || {},
+      cookie,
     })
       .then((res) => {
         const { body } = res;
         if (body?.status === 1 && body?.data) {
           res.cookie.push(`dfid=${res.body.data['dfid']}`);
         }
-
+        res.body.data = {
+          ...(res.body.data || {}),
+          mid: cookie?.KUGOU_API_MID,
+          guid: cookie?.KUGOU_API_GUID,
+          serverDev: cookie?.KUGOU_API_DEV,
+          mac: cookie?.KUGOU_API_MAC,
+        };
         resolve(res);
       })
       .catch((e) => reject(e));
