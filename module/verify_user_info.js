a7d704f9624193df93098db2542fe918275a9d4d
lines <youxi_lin@126.com>
2026-06-13 01:54:41 +0800

    添加支持手机验证码验证

--- module/verify_user_info.js
+++ module/verify_user_info.js
@@ -1,21 +1,40 @@
-const { cryptoAesEncrypt, rsaEncrypt2 } = require('../util');
+const { cryptoAesEncrypt, rsaEncrypt2, cryptoRSAEncrypt } = require('../util');
 // 验证验证码数据
 module.exports = (params, useAxios) => {
   const clienttime = Date.now();
-  // const encrypt = cryptoAesEncrypt(JSON.stringify({}));
-  const dataMap = {
+  const v_type = Number(params?.v_type || 23);
+  //
+  let dataMap = {
     eventid: params?.eventid,
     userid: params?.userid || 0,
     platid: params?.platid || 2,
-    v_type: params?.v_type || 23,
-    verifycode: decodeURIComponent(params?.verifycode || ''),
+    v_type,
     wasm: 1,
     i: '',
     sid: decodeURIComponent(params?.sid || ''),
     edt: decodeURIComponent(params?.edt || ''),
-    // pk: rsaEncrypt2({ clienttime, key: encrypt.key }),
-    // params: encrypt.str,
   };
+
+  if (v_type === 23) {
+    const encrypt = cryptoAesEncrypt({});
+    dataMap = {
+      ...dataMap,
+      verifycode: decodeURIComponent(params?.verifycode || ''),
+      pk: cryptoRSAEncrypt({ key: encrypt.key }),
+      params: encrypt.str,
+    };
+  }
+
+  if (v_type === 32) {
+    const encrypt = cryptoAesEncrypt({ code: params?.verifycode || '' });
+    dataMap = {
+      ...dataMap,
+      code: decodeURIComponent(params?.verifycode || ''),
+      pk: cryptoRSAEncrypt({ key: encrypt.key }),
+      params: encrypt.str,
+    };
+  }
+
 
   return useAxios({
     baseURL: 'https://verifyservice.kugou.com',
