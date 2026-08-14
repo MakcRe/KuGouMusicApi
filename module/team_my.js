d8c93358d9d6cce5fe5d913501a57ca299907ea0
AZLight <1153683020@qq.com>
2026-08-14 08:44:13 +0800

    feat:添加组队相关接口 (#235)

---
+++ module/team_my.js
@@ -0,0 +1,72 @@
+//创建我的队伍
+const crypto = require('crypto');
+const { cryptoMd5,appid,clientver,srcappid,publicLiteRasKey } = require('../util');
+
+function rsaNoPadEncrypt(data, publicKeyPem) {
+    const key = crypto.createPublicKey(publicKeyPem);
+    const encrypted = crypto.publicEncrypt(
+        {
+            key: key,
+            padding: crypto.constants.RSA_NO_PADDING,
+        },
+        data
+    );
+    return encrypted.toString('hex');
+}
+
+//携带请求体进行签名
+function signatureWebParamsWithBody(params, bodyStr) {
+    const SALT = 'NVPh5oo715z5DIWAeQlhMDsWXXQV4hwt';
+    const paramsString = Object.keys(params)
+        .sort()
+        .map(k => `${k}=${params[k]}`)
+        .join('');
+    const input = SALT + paramsString + bodyStr + SALT;
+    return cryptoMd5(input);
+}
+
+module.exports = (params, useAxios) => {
+
+    const rawToken = params?.token || params?.cookie?.token || '';
+    const userid = Number(params?.userid || params?.cookie?.userid || '0');
+    const mid = params?.cookie?.KUGOU_API_MID || params?.mid || '';
+    const dfid = params?.dfid || params?.cookie?.dfid || '-';
+    const uuid = params?.uuid || params?.cookie?.uuid || '-';
+
+    let token = rawToken;
+    const prefix = 'moc.uoguk.59::';                
+    const input = Buffer.from(prefix + rawToken, 'utf8');
+    const padded = Buffer.alloc(128);
+    input.copy(padded);
+
+    const encrypted = rsaNoPadEncrypt(padded, publicLiteRasKey).toUpperCase();
+    token = 'h5' + encrypted;
+    const clienttime = Date.now();
+    const paramsMap = {
+        srcappid,
+        clientver,
+        clienttime,
+        mid,
+        uuid,
+        dfid,
+        appid,
+        userid,
+        token,
+    };
+
+    const dataMap = { period_id: params.period_id };
+    //转为字符串参与签名
+    const dataStr = JSON.stringify(dataMap);
+
+    const signature = signatureWebParamsWithBody(paramsMap, dataStr);
+    const finalParams = { ...paramsMap, signature };
+
+    return useAxios({
+        url: '/youth/v1/ut/create_team',
+        method: 'POST',
+        data: dataMap,
+        params: finalParams,
+        cookie: params?.cookie || {},
+        headers: { 'Host': 'gateway.kugou.com' },
+    });
+};
